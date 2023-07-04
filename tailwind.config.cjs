43e5b5ccf6db0b30c4dcecf53cb62c5fd043c6a5
Rob Lehmann <lehmannrobb@gmail.com>
2023-07-04 16:11:02 +0300

    Added layout and updated theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        lightBlue: "#1DA1F2",
+        lightGray: "#f4f4f4",
+        gray: "#718C9E",
+        dark: "#242526",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
